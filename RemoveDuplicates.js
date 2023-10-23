f179f47d27ed500168227b44853cd7faf2af06d4
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-10-23 17:07:32 +0530

    updates

---
+++ RemoveDuplicates.js
@@ -0,0 +1,16 @@
+const nums = [0,0,1,1,1,2,2,3,3,4];
+
+const removeDuplicates = (nums) => {
+    let k = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+console.log(removeDuplicates(nums))
